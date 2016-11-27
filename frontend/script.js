195534231138a0fa8cc39019d705d498d96fc13e
Vlad Golubev <golubev.vld@gmail.com>
2016-11-28 01:05:15 +0200

    Add inputs for rooms dimensions

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,8 @@
 window.onload = () => {
+  $('[data-action=add-room]').on('click', () => {
+    const $roomForm = $('#room-form');
+    const $roomEntry = $roomForm.find('.room-entry').last();
 
+    $roomForm.append($roomEntry.html());
+  });
 };
