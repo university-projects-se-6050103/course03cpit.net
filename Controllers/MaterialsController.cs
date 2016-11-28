95f3b40ccf6c6a5aa732d58dcb33d72b5935298f
Vlad Golubev <golubev.vld@gmail.com>
2016-11-29 00:44:05 +0200

    Remove unused REST methods from MaterialsController

--- Controllers/MaterialsController.cs
+++ Controllers/MaterialsController.cs
@@ -20,23 +20,5 @@ namespace apartment_renovation_cost.Controllers
                 .Where(material => material.category == category)
                 .Select(material => material.name);
         }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
